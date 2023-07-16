643331de508b762dfc2b48f0fc43f1325f8f9242
Lim <lshare@MacBook-Pro.local>
2023-07-16 23:30:08 +0800

    @mdi/font use CDN

--- web/src/main.js
+++ web/src/main.js
@@ -7,7 +7,6 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import "@mdi/font/css/materialdesignicons.css";
 import { aliases, mdi } from "vuetify/lib/iconsets/mdi";
 
 import { createRouter, createWebHashHistory } from 'vue-router';
